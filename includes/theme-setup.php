32060d314450285683b64ab24e5e8c0137977c50
Mike Selander <mikeselander@gmail.com>
2016-01-15 17:20:19 -0700

    Fixed missing semicolon in theme-setup && unneeded roles in search form

--- includes/theme-setup.php
+++ includes/theme-setup.php
@@ -35,7 +35,7 @@ class ThemeSetup{
 
 		add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'automatic-feed-links' );
-		add_theme_support( 'title-tag' )
+		add_theme_support( 'title-tag' );
 		add_theme_support( 'html5', array(
 			'search-form',
 			'gallery',
